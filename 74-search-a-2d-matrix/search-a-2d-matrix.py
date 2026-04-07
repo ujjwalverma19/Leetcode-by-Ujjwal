5bec71a3c564aa4a855ae208028d3441f27eeb3b
Ujjwal Verma <ujjwal.verma1905@gmail.com>
2026-04-07 14:55:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (55.61%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        m, n = len(matrix), len(matrix[0])
+        
+        left, right = 0, m * n - 1
+        while left <= right:
+            mid = (left + right) // 2        
+            row = mid // n
+            col = mid % n           
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                left = mid + 1
+            else:
+                right = mid - 1  
+        return False
