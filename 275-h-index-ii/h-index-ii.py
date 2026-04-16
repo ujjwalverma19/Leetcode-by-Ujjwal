51d2a330976c6d7195b0353e2d851e3280904718
Ujjwal Verma <ujjwal.verma1905@gmail.com>
2026-04-16 10:42:21 +0530

    Time: 0 ms (100.00%) | Memory: 24.6 MB (40.15%) - LeetSync

--- 275-h-index-ii/h-index-ii.py
+++ 275-h-index-ii/h-index-ii.py
@@ -1,13 +1,12 @@
 class Solution:
-    def hIndex(self, citations):
-        n = len(citations)
-        left, right = 0, n - 1
-        while left <= right:
-            mid = (left + right) // 2
-            papers = n - mid
-
-            if citations[mid] >= papers:
-                right = mid - 1
+    def hIndex(self,citations):
+        n=len(citations)
+        left,right=0,n-1
+        while left<=right:
+            mid=(left+right)//2
+            paper=n-mid
+            if citations[mid]>=paper:
+                right=mid-1
             else:
-                left = mid + 1
-        return n - left
+                left=mid+1
+        return n-left
