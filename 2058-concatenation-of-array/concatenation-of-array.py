948897427bddc70f8d1ef88eac3204f5f6e04848
Ujjwal Verma <ujjwal.verma1905@gmail.com>
2026-05-26 23:49:58 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (40.54%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        output=[]
+        for i in nums:
+            output.append(i)
+        for i in nums:
+            output.append(i)
+        return output
